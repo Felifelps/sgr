477fa4f6ac8c6231fb7e4bc2e2ed6cf90d45df8a
Felifelps <felifelps.dev@gmail.com>
2025-08-23 12:57:44 -0300

    chore: Project Setup

---
+++ src/main/java/com/sgr/App.java
@@ -0,0 +1,9 @@
+package com.sgr;
+
+import com.sgr.ui.TerminalInterface;
+
+public class App {
+    public static void main(String[] args) {
+        TerminalInterface.run();
+    }
+}
---
+++ src/main/java/com/sgr/ui/TerminalInterface.java
@@ -0,0 +1,8 @@
+package com.sgr.ui;
+
+public class TerminalInterface {
+
+    public static void run() {
+        System.out.println("Hello world!");
+    }
+}
